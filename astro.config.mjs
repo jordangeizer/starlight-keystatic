4bc9a4b45e2bbcc726a4a2f21ad09308610e0c27
jordangeizer <19618621+jordangeizer@users.noreply.github.com>
2024-09-09 12:38:25 +1000

    move integration order

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,23 +13,6 @@ export default defineConfig({
 	integrations: [
 		keystatic(),
     react(),
-		starlight({
-			title: 'Keystatic Starlight',
-      logo: {
-        light: './src/assets/light-logo.svg',
-        dark: './src/assets/dark-logo.svg',
-        replacesTitle: true,
-      },
-			/* 
-        The server needs to be manually restarted whenever 
-        the sidebarData below is changed, since nothing 
-        on this file is changing.
-
-        Is there a way to "watch" a specific file for 
-        change and trigger a server restart?
-      */
-      // sidebar: sidebarData,
-		}),
 		AutoImport({
       imports: [
         {
@@ -49,5 +32,22 @@ export default defineConfig({
         },
       ],
     }),
+		starlight({
+			title: 'Keystatic Starlight',
+      logo: {
+        light: './src/assets/light-logo.svg',
+        dark: './src/assets/dark-logo.svg',
+        replacesTitle: true,
+      },
+			/* 
+        The server needs to be manually restarted whenever 
+        the sidebarData below is changed, since nothing 
+        on this file is changing.
+
+        Is there a way to "watch" a specific file for 
+        change and trigger a server restart?
+      */
+      // sidebar: sidebarData,
+		}),
 	],
 });
